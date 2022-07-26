7201e0e9f495a445fcaa7c78a46db5caef0e9643
RubenU2002 <rsantiagourrego@gmail.com>
2022-07-26 18:10:16 -0500

    modal y codigo bonito

--- insertarEnCursos/inscribirEst.php
+++ insertarEnCursos/inscribirEst.php
@@ -1,33 +1,4 @@
 <?php 
     include_once("../conexion.php");
+    
  ?>
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.min.css">
-    <title>Document</title>
- </head>
- <body>
-    <div class="m-0 vh-100 row justify-content-center align-items-center">
-        <div class="col-auto p-5 text-center">
-            <form action="/insertarEnCursos/inscribirEst.php" method="POST">
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Seleccionar estudiante</option>
-                    <?php
-                        $consulta=pg_query("select * from estudiantes");
-                        while($objEstu=pg_fetch_object($consulta)){
-                        ?>
-                            <option><?php echo $objEstu->cod_est," | ",$objEstu->nomb_est; ?></option>
-                        <?php
-                        }
-                    ?>
-                </select>
-            </form>
-        </div>
-    </div>
-    <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
- </body>
- </html>
--- insetarEst.php
+++ insetarEst.php
@@ -9,23 +9,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
-    <title>InsertarEstuiante</title>
+    <title>InsertarEstudiante</title>
 </head>
 <body >
-    
-    <div class="m-0 vh-100 row justify-content-left align-items-left">
-        <div class="col-auto p-5 text-center">
-            <form action="insertarEnCursos/inscribirEst.php">
-                <button type="submit" class="btn btn-success">
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+        <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
+        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"></path>
+    </svg>
+  Inscribir Estudiante
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Inscribir estudiante</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="insertarEnCursos/inscribirEst.php">
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Seleccionar estudiante</option>
+                    <?php
+                        $consulta=pg_query("select * from estudiantes");
+                        while($objEstu=pg_fetch_object($consulta)){
+                        ?>
+                            <option value="<?php echo $row1->cod_est?>"><?php echo $objEstu->nomb_est," | ",$objEstu->cod_est; ?></option>
+                        <?php
+                        }
+                    ?>
+                </select>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-success">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
                         <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"></path>
                         <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"></path>
                     </svg>
-                    Agregar estudiante
-                </button>
-            </form>
-        </div>
+                inscribir
+            </button>
+            </div>
+        </form>
+      </div>
     </div>
+  </div>
+</div>
 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
  </body>
  </html>
