2b50b084f63e3251a959558272938441c00a360b
RubenU2002 <rsantiagourrego@gmail.com>
2022-07-30 02:46:08 -0500

    definitivas funcionando (def por curso año y perio

--- notasycalificaciones/reporteDeNotas.php
+++ notasycalificaciones/reporteDeNotas.php
@@ -56,7 +56,7 @@
                         while($mostrarSec=pg_fetch_object($secNotas)){?>
                             <td><?php echo $mostrarSec->valor ?></td>
                     <?php }?>
-                    <?php $queryprom= pg_query("select sum(p.prom) from(select (valor*n.porcentaje/(10))/10 as prom,c.cod_est from calificaciones c, notas n where n.nota=c.nota and cod_est='$mostrarSecCal->cod_est') p");
+                    <?php $queryprom= pg_query("select sum(p.prom) from(select (valor*n.porcentaje/(10))/10 as prom,c.cod_est from calificaciones c, notas n where n.nota=c.nota and cod_est='$mostrarSecCal->cod_est' and c.cod_cur='$curso' and c.year='$year' and c.periodo='$periodo') p");
                     $def=pg_fetch_object($queryprom);
                     ?>
                     <td> <?php echo $def->sum ?></td>
