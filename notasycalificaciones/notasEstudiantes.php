a47048b642a6c42e8527171ca1f3bdcf245ade48
RubenU2002 <rsantiagourrego@gmail.com>
2022-07-30 02:03:37 -0500

    notas

--- notasycalificaciones/notasEstudiantes.php
+++ notasycalificaciones/notasEstudiantes.php
@@ -87,7 +87,9 @@
       </tfoot>
     </table>
   </div>
-    
+  <form action="reporteDeNotas.php">
+      <button type="submit">Reporte de notas</button>
+    </form>
   <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
