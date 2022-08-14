75d1035bd95d6fb100aee6ca4714dfea8954cfff
RubenU2002 <rsantiagourrego@gmail.com>
2022-08-14 00:51:38 -0500

    SIN SEQUENCE

--- modificaciones/eliminarNota.php
+++ modificaciones/eliminarNota.php
@@ -6,7 +6,7 @@
     echo $nota;
     echo $curso;
     pg_query("delete from notas cascade where nota='$nota' and cod_cur='$curso'");
-    pg_query("alter sequence calificaciones_cod_cal_seq restart with 1;");
-    pg_query("update calificaciones set cod_cal=nextval('calificaciones_cod_cal_seq');");
+    //pg_query("alter sequence calificaciones_cod_cal_seq restart with 1;");
+    //pg_query("update calificaciones set cod_cal=nextval('calificaciones_cod_cal_seq');");
     header("location:../notasycalificaciones/insertarNotas.php");
  ?>
